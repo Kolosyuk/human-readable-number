1b33d402e6622f7e0055db6351484c7e65564d43
Kolosyuk <crplutt@gmail.com>
2020-10-05 00:56:44 +0300

    refactor: clittle changes in algorithm

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 module.exports = function toReadable(number) {
-  let collection = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+  const collection = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let collection10 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  let hundred = 'hundred';
-  if (number.toString().length === 1) return collection[number]
-  if (number.toString().length === 3) {
-    if (/[^00$]/.test(number.toString().substring(1))) {
-      return `${collection[number.toString()[0]]} ${hundred} ${toReadable(number.toString().substring(1))}`
+  const collection10 = ['0', '0', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const strNumber = number.toString();
+  if (number < 20) return collection[number]
+  if (strNumber.length === 3) {
+    if (/[^00$]/.test(strNumber.substring(1))) {
+      return `${collection[strNumber[0]]} hundred ${toReadable(Number.parseInt(strNumber.substring(1)))}`
     } else {
-      return `${collection[number.toString()[0]]} ${hundred}`
+      return `${collection[strNumber[0]]} hundred`
     }
   }
-  if (number.toString().length === 2 && number >= 20) {
-    if (number % 10 === 0) return `${collection10[number.toString()[0] - 2]}`
-    return `${collection10[number.toString()[0] - 2]} ${toReadable(number.toString().substring(1))}`
-  } else if (number >= 10) {
-    return `${collection[number]}`
-  } else {
-    return `${toReadable(number.toString().substring(1))}`
-  }
+  if (number % 10 === 0) return `${collection10[strNumber[0]]}`
+  return `${collection10[strNumber[0]]} ${toReadable(strNumber.substring(1))}`
 }
