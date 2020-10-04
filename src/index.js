4da9c4d9a42f9efafe840a7d4b35f2acf07b73a9
Kolosyuk <crplutt@gmail.com>
2020-10-05 00:31:27 +0300

    init: human-readable-number task. DONE>>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  let collection = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let collection10 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundred = 'hundred';
+  if (number.toString().length === 1) return collection[number]
+  if (number.toString().length === 3) {
+    if (/[^00$]/.test(number.toString().substring(1))) {
+      return `${collection[number.toString()[0]]} ${hundred} ${toReadable(number.toString().substring(1))}`
+    } else {
+      return `${collection[number.toString()[0]]} ${hundred}`
+    }
+  }
+  if (number.toString().length === 2 && number >= 20) {
+    if (number % 10 === 0) return `${collection10[number.toString()[0] - 2]}`
+    return `${collection10[number.toString()[0] - 2]} ${toReadable(number.toString().substring(1))}`
+  } else if (number >= 10) {
+    return `${collection[number]}`
+  } else {
+    return `${toReadable(number.toString().substring(1))}`
+  }
 }
